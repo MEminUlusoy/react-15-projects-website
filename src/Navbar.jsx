47c539a0151ffe61af9916cb19223c915ce16e73
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-10-29 16:12:00 +0300

    finished project

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,11 +10,11 @@ function Navbar() {
         <div className='h-full w-2/4 md:w-2/4 lg:w-2/5  flex justify-around  sm:justify-between md:justify-around pr-2 sm:pr-5 '>
             <div className='hidden sm:flex items-center text-white font-f1 font-bold'>
                 <i className="fa-solid fa-phone        text-xs    sm:text-sm  md:text-base  2xl:text-base"></i>
-                <p className='sm:ml-2 md:ml-3 2xl:ml-5 text-xs  sm:text-xs  md:text-sm    2xl:text-base'>+90 507 530 4850</p>
+                <p className='sm:ml-2 md:ml-3 2xl:ml-3 text-xs  sm:text-xs  md:text-sm    2xl:text-base'>+90 507 530 4850</p>
             </div>
             <div className='flex items-center text-white font-f1 font-bold'>
                 <i className="fa-solid fa-envelope     text-xs   sm:text-sm md:text-base  2xl:text-base"></i>
-                <p className='ml-2 sm:ml-2 md:ml-3 2xl:ml-5 text-xs sm:text-xs md:text-sm    2xl:text-base'>ulusoyeminn@gmail.com</p>
+                <p className='ml-2 sm:ml-2 md:ml-3 2xl:ml-3 text-xs sm:text-xs md:text-sm    2xl:text-base'>ulusoyeminn@gmail.com</p>
             </div>
             
         </div>
